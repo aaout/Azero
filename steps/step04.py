b66c9e013db968220d0a2c53bbfb1dcc0a108ea1
aaout <tiktik_suke@outlook.jp>
2020-09-06 17:41:44 +0900

    微分関数の修正

--- steps/step04.py
+++ steps/step04.py
@@ -9,7 +9,7 @@ def numelical_diff(f, x, eps=1e-4):
     x1 = Variable(x.data + eps)
     y0 = f(x0)
     y1 = f(x1)
-    return (y1.data - y0.data) / 2 * eps
+    return (y1.data - y0.data) / (2 * eps)
 
 if __name__ == "__main__":
     x = Variable(np.array(2))
